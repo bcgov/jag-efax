a8dfa5b45cb9adbe7210ad9c6fa276cd86557fec
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-06-15 09:54:31 -0700

    EFAX-25 added javadocs to controller

--- mailservice/src/main/java/ca/bc/gov/ag/mail/MailController.java
+++ mailservice/src/main/java/ca/bc/gov/ag/mail/MailController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ca.bc.gov.ag.exception.MailException;
 
+/**
+ * This {@link MailController} exposes the {@link MailService} in a REST API.
+ */
 @RestController
 @RequestMapping("/v1")
 public class MailController {
@@ -23,6 +26,13 @@ public class MailController {
 	@Autowired
 	private MailService mailService;
 	
+	/**
+	 * This method accepts a {@link MailMessage} as a POST request to create and send an email using Microsoft Exchange server.
+	 * 
+	 * @param mailMessage contains a uuid, to, subject, and body components, as well as a list of URIs (Strings) to include
+	 *                    as attachments.
+	 * @throws MailException if any error occurs during delivery.
+	 */
 	@PostMapping(path = "/sendMessage", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void sendMessage(@RequestBody MailMessage mailMessage) throws MailException {
 		logger.debug("Request to /api/v1/sendMessage, uuid: {}", mailMessage.getUuid());
