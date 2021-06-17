0d55e6118ee486051b5639fbcc9778ac0b90104b
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-06-17 11:43:27 -0700

    EFAX-22 initiate endpoint has no return value

--- doc-distrib-ws/src/main/java/ca/bc/gov/ag/dist/ws/DocumentDistributionEndpoint.java
+++ doc-distrib-ws/src/main/java/ca/bc/gov/ag/dist/ws/DocumentDistributionEndpoint.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,16 +15,11 @@ public class DocumentDistributionEndpoint {
 	private Logger logger = LoggerFactory.getLogger(DocumentDistributionEndpoint.class);
 
 	@PayloadRoot(namespace = WebServiceConfig.NAMESPACE_URI, localPart = "DocumentDistributionRequest")
-	@ResponsePayload
-	public DocumentDistributionMainProcessProcessResponse getCountry(@RequestPayload DocumentDistributionRequest request) {
-		DocumentDistributionMainProcessProcessResponse response = new DocumentDistributionMainProcessProcessResponse();
-
+	public void initiate(@RequestPayload DocumentDistributionRequest request) {
 		try {
 			String serialized = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(request);
 			logger.trace(serialized);
 		} catch (JsonProcessingException e) {}
-		
-		return response;
 	}
 	
 }
