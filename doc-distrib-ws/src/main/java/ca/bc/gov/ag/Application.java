f829b143f0e017ee7e6a2b2e46bc344b87f24070
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-06-25 10:29:39 -0700

    EFAX-22 added a scheduler for dealing with fax timeouts

--- doc-distrib-ws/src/main/java/ca/bc/gov/ag/Application.java
+++ doc-distrib-ws/src/main/java/ca/bc/gov/ag/Application.java
@@ -1,9 +1,11 @@
-package ca.bc.gov.ag.dist;
+package ca.bc.gov.ag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
