9a176383d93a21b81e9f6f9977687e844169cbfb
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-06-29 12:15:52 -0700

    Added a scheduler to poll for exhange emails

---
+++ fax-listener/src/main/java/ca/bc/gov/ag/fl/scheduler/EmailPoller.java
@@ -0,0 +1,33 @@
+package ca.bc.gov.ag.fl.scheduler;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import ca.bc.gov.ag.fl.service.EmailService;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+
+@Component
+public class EmailPoller {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    @Autowired
+    private EmailService emailService;
+    
+    @Scheduled(fixedDelayString = "${exchange.poller.interval}")
+    public void pollForEmails() throws Exception {
+        logger.debug("Started email indox poll.");
+        
+        List<EmailMessage> emailMessages = emailService.getEfaxInboxEmails();
+        
+        logger.debug("Retrieved {} emails", emailMessages.size());
+        
+        // TODO: EFAX-24 parse emails, looking for jobId and status of the sent fax
+    }
+    
+}
