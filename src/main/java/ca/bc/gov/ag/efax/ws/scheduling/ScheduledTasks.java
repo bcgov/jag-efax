6928c9ef491a630a4f6c605e943762e577a62101
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-07-06 14:31:35 -0700

    efax-22 stubbed out Justin SOAP callback

--- src/main/java/ca/bc/gov/ag/efax/ws/scheduling/ScheduledTasks.java
+++ src/main/java/ca/bc/gov/ag/efax/ws/scheduling/ScheduledTasks.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import ca.bc.gov.ag.dist.efax.ws.model.DocumentDistributionMainProcessProcessUpdate;
+import ca.bc.gov.ag.dist.efax.ws.model.DocumentDistributionMainProcessProcessResponse;
 import ca.bc.gov.ag.efax.mail.model.SentMessage;
 import ca.bc.gov.ag.efax.mail.repository.SentMessageRepository;
 import ca.bc.gov.ag.efax.ws.exception.FAXTimeoutFault;
@@ -29,9 +29,9 @@ public class ScheduledTasks {
     @Autowired
     private DocumentDistributionService documentDistributionService;
     
-    @Scheduled(fixedDelayString = "${ws.faxTimeoutCheck}")
+    @Scheduled(fixedDelayString = "${ws.faxTimeoutPoll}")
     public void sentFaxTimeout() {
-        // Every ${ws.faxTimeoutCheck} milliseconds, check if there are any queued SentMessages in redis whose createdTimestamp is older than ${ws.faxTimeout} milliseconds.
+        // Every ${ws.faxTimeoutPoll} milliseconds, check if there are any queued SentMessages in redis whose createdTimestamp is older than ${ws.faxTimeout} milliseconds.
         // If so, then remove the record from redis and send a callback to the client indicating the message timed out.
 
         logger.trace("Checking for timed out fax messages");
@@ -44,13 +44,16 @@ public class ScheduledTasks {
                 logger.debug("Sent message uuid:{} timed out", sentMessage.getUuid());
 
                 // send a callback to the client
-                documentDistributionService.sendResponseToCallback(getResponse(sentMessage.getJobId()));
-                
-                // TODO: if the callback fails to send, should we add a retry counter to the redis object and try again later, perhaps failing after 3 retries?
+                try {                    
+                    documentDistributionService.sendResponseToCallback(getResponse(sentMessage.getJobId()));
+                }
+                catch (Exception e) {
+                    logger.error("Could not invoke a Justin callback for jobId: {}", sentMessage.getJobId());
+                    logger.error(e.getMessage(), e);
+                }
                 
                 // remove message from queue
-                sentMessageRepository.deleteById(sentMessage.getUuid());
-                
+                sentMessageRepository.deleteById(sentMessage.getUuid());                
             }
         }
     }
@@ -58,12 +61,12 @@ public class ScheduledTasks {
     /**
      * Returns the error response object to send to Justin's SOAP callback service.
      */
-    private DocumentDistributionMainProcessProcessUpdate getResponse(String jobId) {
-        DocumentDistributionMainProcessProcessUpdate response = new DocumentDistributionMainProcessProcessUpdate();
+    private DocumentDistributionMainProcessProcessResponse getResponse(String jobId) {
+        DocumentDistributionMainProcessProcessResponse response = new DocumentDistributionMainProcessProcessResponse();
         response.setJobId(jobId);
         FAXTimeoutFault fault = new FAXTimeoutFault();
-        response.setStatus(fault.getFaultCode());
-        response.setStatusMsg(fault.getFaultMessage());
+        response.setStatusCode(fault.getFaultCode());
+        response.setStatusMessage(fault.getFaultMessage());
         return response;
     }
     
