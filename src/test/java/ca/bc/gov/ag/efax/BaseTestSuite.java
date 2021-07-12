8f42e9ddab85e26bfe9791eccf8283da38179524
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-07-12 14:16:21 -0700

    Added doc distrib service junit test

--- src/test/java/ca/bc/gov/ag/efax/BaseTestSuite.java
+++ src/test/java/ca/bc/gov/ag/efax/BaseTestSuite.java
@@ -1,17 +1,84 @@
 package ca.bc.gov.ag.efax;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.event.annotation.AfterTestExecution;
 
+import ca.bc.gov.ag.efax.mail.repository.SentMessageRepository;
+import ca.bc.gov.ag.efax.mail.service.ExchangeServiceFactory;
+import ca.bc.gov.ag.efax.pdf.service.PdfService;
 import ca.bc.gov.ag.efax.redis.TestRedisConfiguration;
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.response.CreateAttachmentResponse;
+import microsoft.exchange.webservices.data.core.response.ServiceResponseCollection;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.property.complex.ItemId;
+import redis.embedded.RedisServer;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestRedisConfiguration.class)
 @ActiveProfiles("test")
 public abstract class BaseTestSuite {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    protected RedisServer redisServer;
+
     @Autowired
     protected TestRestTemplate restTemplate;
+
+    @MockBean
+    protected ExchangeService exchangeService;
+
+    @Autowired
+    protected SentMessageRepository sentMessageRepository;
+
+    @MockBean
+    private ExchangeServiceFactory exchangeServiceFactory;
+
+    @MockBean
+    protected PdfService pdfService;
     
+    @BeforeEach
+    protected void beforeEach() throws Exception {
+        if (!redisServer.isActive()) {
+            logger.info("Redis is starting...");
+            redisServer.start();
+        }
+        
+        when(exchangeServiceFactory.createService()).thenReturn(exchangeService);
+
+        // mock basic exchangeService methods
+        when(exchangeService.getRequestedServerVersion()).thenReturn(ExchangeVersion.Exchange2010_SP2);
+        when(exchangeService.createAttachments(any(), any())).thenReturn(new ServiceResponseCollection<CreateAttachmentResponse>());
+
+        // exchangeService - add a generated UUID when "saving" an email message
+        doAnswer(invocation -> {
+            EmailMessage emailMessage = invocation.getArgument(0);
+            emailMessage.getPropertyBag().getProperties().put(emailMessage.getIdPropertyDefinition(), new ItemId(UUID.randomUUID().toString()));
+            return null;
+        }).when(exchangeService).createItem(any(), any(), any(), any());
+    }
+
+    @AfterTestExecution
+    protected void afterTestExecution() throws Exception {
+        try {
+            logger.info("Redis is stopping...");
+            redisServer.stop();
+        } catch (Exception e) {
+        }
+    }
 }
---
+++ src/test/java/ca/bc/gov/ag/efax/ws/service/DocumentDistributionServiceTest.java
@@ -0,0 +1,103 @@
+package ca.bc.gov.ag.efax.ws.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ca.bc.gov.ag.dist.efax.ws.model.DocumentDistributionRequest;
+import ca.bc.gov.ag.dist.efax.ws.model.DocumentDistributionRequest.Attachments;
+import ca.bc.gov.ag.efax.BaseTestSuite;
+import ca.bc.gov.ag.efax.mail.util.DateUtil;
+import ca.bc.gov.ag.efax.ws.exception.UnknownChannelFault;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+
+public class DocumentDistributionServiceTest extends BaseTestSuite {
+
+    @Autowired
+    private DocumentDistributionService documentDistributionService;
+
+    private List<EmailMessage> emails;
+
+    @BeforeEach
+    @Override
+    protected void beforeEach() throws Exception {
+        super.beforeEach();
+
+        // An in-memory cache of "sent" emails by the ExchangeService
+        emails = new ArrayList<EmailMessage>();
+
+        // exchangeService - add the email to the List<EmailMessage> when "sending" an email.
+        doAnswer(invocation -> {
+            EmailMessage emailMessage = invocation.getArgument(0);
+            emails.add(emailMessage);
+            return null;
+        }).when(exchangeService).sendItem(any(), any());
+
+        // disable flattening service, return null
+        when(pdfService.flattenPdf(any(), any())).thenReturn(null);
+    }
+
+    @Test
+    public void testReceiveRequestToSendMessage() throws Exception {
+        // Happy path - sending a correctly formated request should 
+        // - "send" an email
+        // - add a waiting queue record to redis
+
+        // assert emails and redis are empty
+        assertEquals(0, emails.size());
+        assertEquals(0, sentMessageRepository.count());       
+        
+        documentDistributionService.receiveRequestToSendMessage(createBaseRequest());
+
+        // assert emails and redis now have 1 record each.
+        assertEquals(1, emails.size());
+        assertEquals(1, sentMessageRepository.count());      
+        
+        assertEquals("Body text", emails.get(0).getBody().toString());
+        assertEquals(1, emails.get(0).getAttachments().getCount());
+    }
+
+    @Test
+    public void testReceiveMustBeAFax() throws DatatypeConfigurationException {
+        // Attempt to send a message of type "email" - should fail as only "fax" is implemented.
+
+        DocumentDistributionRequest request = createBaseRequest();
+        request.setChannel("email");
+        try {
+            documentDistributionService.receiveRequestToSendMessage(request);
+            fail("Channel must be set to fax and only fax.");
+        } catch (UnknownChannelFault e) {
+        }
+
+        // assert no emails sent
+        assertEquals(0, emails.size());
+    }
+
+    /** Creates a basic fax request message. */
+    private DocumentDistributionRequest createBaseRequest() throws DatatypeConfigurationException {
+        DocumentDistributionRequest request = new DocumentDistributionRequest();
+        request.setJobId("12345");
+        request.setTo("Someone at The Office");
+        request.setSubject("Subject text");
+        request.setBody("Body text");
+        request.setChannel("Fax");
+        request.setTransport("2505551234");
+        request.setAttachments(new Attachments());
+        request.getAttachments().getUri().add(getClass().getClassLoader().getResource("sample_v1.6.pdf").toString());
+        request.setDateTime(DateUtil.toXMLGregorianCalendar(new Date()));
+        return request;
+    }
+
+}
