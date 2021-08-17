2b86d12cf275c92ffce7864ef58b3512bdda8b3b
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2021-08-17 08:13:43 -0700

    fixed guid string in test

--- src/test/java/ca/bc/gov/ag/efax/mail/service/parser/SucceededVisitorTest.java
+++ src/test/java/ca/bc/gov/ag/efax/mail/service/parser/SucceededVisitorTest.java
@@ -14,7 +14,7 @@ public class SucceededVisitorTest extends BaseTestSuite {
     void testApply_Success() throws Exception {
         SucceededVisitor visitor = new SucceededVisitor();
         String subject = "Message Succeeded: 2505551234 (2505551234@somewhere.com) on 8/13/2021 at 12:38:15 PM";
-        String body = "Your fax which was sent to 2505551234@somewhere.com at 2505551234 with subject <jobId>1234</jobId><uuid>8e3b47c4-60b6-43e7-81ee- c9ac5930e49d</uuid> was delivered successfully";
+        String body = "Your fax which was sent to 2505551234@somewhere.com at 2505551234 with subject <jobId>1234</jobId><uuid>8e3b47c4-60b6-43e7-81ee-c9ac5930e49d</uuid> was delivered successfully";
         DocumentDistributionMainProcessProcessResponseDecorator response = new DocumentDistributionMainProcessProcessResponseDecorator();
         visitor.apply(subject, body, response);
         
@@ -29,7 +29,7 @@ public class SucceededVisitorTest extends BaseTestSuite {
     void testApply_Failed() throws Exception {
         SucceededVisitor visitor = new SucceededVisitor();
         String subject = "Message Failed: 2505551234 - Failed (The transmission was disconnected while in progress.)";
-        String body = "Your fax which was sent to 2505551234@somewhere.com at 2505551234 with subject <jobId>1234</jobId><uuid>070d08c0-8704-4232-b5e2- cc23b377433f</uuid> on 8/13/2021 at 11:17:41 AM, failed to be delivered";
+        String body = "Your fax which was sent to 2505551234@somewhere.com at 2505551234 with subject <jobId>1234</jobId><uuid>070d08c0-8704-4232-b5e2-cc23b377433f</uuid> on 8/13/2021 at 11:17:41 AM, failed to be delivered";
         DocumentDistributionMainProcessProcessResponseDecorator response = new DocumentDistributionMainProcessProcessResponseDecorator();
         visitor.apply(subject, body, response);
 
