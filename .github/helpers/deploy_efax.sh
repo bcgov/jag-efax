89af792e71a039cd31c8779d47f308b6e0f59b3f
Adam Kroon <adam.kroon@nttdata.com>
2021-08-18 10:18:40 -0700

    Update deploy_efax.sh

--- .github/helpers/deploy_efax.sh
+++ .github/helpers/deploy_efax.sh
@@ -4,5 +4,5 @@ oc process -f dc.yaml -n ${{ secrets.OPENSHIFT_DEV_NAMESPACE }} \
     -p APPLICATION_NAME=${{ secrets.APPLICATION_NAME }} \
     -p LICENSE_PLATE=${{ secrets.LICENSE_PLATE }} \
     -p ENVIRONMENT=${{ secrets.DEV_ENVIRONMENT }} \
-    -p EFAX_IMAGE_VERSION=${env.LATEST_VERSION}| \
+    -p EFAX_IMAGE_VERSION=${{ env.LATEST_VERSION }} | \
     oc4 apply -f -
