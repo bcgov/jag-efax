925621745073401529f0903ca58fa31d06f993b2
Adam Kroon <adam.kroon@nttdata.com>
2021-08-18 11:57:13 -0700

    Update deploy_efax.sh

--- .github/helpers/deploy_efax.sh
+++ .github/helpers/deploy_efax.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-oc process -f dc.yaml -n $1 \
+oc process -f /home/runner/work/jag-efax/jag-efax/openshift/templates/efax-webservice/dc.yaml -n $1 \
     -p APPLICATION_NAME=$2 \
     -p LICENSE_PLATE=$3 \
     -p ENVIRONMENT=$4 \
