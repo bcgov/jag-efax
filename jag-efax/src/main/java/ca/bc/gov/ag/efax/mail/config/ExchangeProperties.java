ffdf704cee65b101411565d4d98988056cd0fec4
TayGov <47829178+TayGov@users.noreply.github.com>
2024-04-25 10:43:55 -0700

    Java 17

--- jag-efax/src/main/java/ca/bc/gov/ag/efax/mail/config/ExchangeProperties.java
+++ jag-efax/src/main/java/ca/bc/gov/ag/efax/mail/config/ExchangeProperties.java
@@ -1,6 +1,6 @@
 package ca.bc.gov.ag.efax.mail.config;
 
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotEmpty;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.util.StringUtils;
--- jag-efax/src/main/java/ca/bc/gov/ag/efax/pdf/config/PdfProperties.java
+++ jag-efax/src/main/java/ca/bc/gov/ag/efax/pdf/config/PdfProperties.java
@@ -1,6 +1,6 @@
 package ca.bc.gov.ag.efax.pdf.config;
 
-import javax.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotEmpty;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
